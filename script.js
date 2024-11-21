fe45d01f0851f254283f7c9c9383c184f02836b0
ibrahimthai <ibrahim.thai@gmail.com>
2024-11-21 14:52:25 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const apiKey = '17c594ccc39c83eafe7e70224eba432e'; // OpenWeatherMap API key
+const searchButton = document.getElementById('searchButton');
+const cityInput = document.getElementById('cityInput');
+const weatherInfo = document.getElementById('weatherInfo');
+const cityName = document.getElementById('cityName');
+const temperature = document.getElementById('temperature');
+const description = document.getElementById('description');
+
+searchButton.addEventListener('click', () => {
+    const city = cityInput.value.trim();
+    if (city) {
+        fetchWeatherData(city);
+    }
+});
+
+async function fetchWeatherData(city) {
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        );
+        if (!response.ok) {
+            throw new Error('Error', 'City not found');
+        }
+        const data = await response.json();
+        displayWeatherData(data);
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+function displayWeatherData(data) {
+    cityName.textContent = data.name;
+    temperature.textContent = `Temperature: ${data.main.temp}°C`;
+    description.textContent = `Weather: ${data.weather[0].description}`;
+    weatherInfo.classList.remove('hidden');
+}
